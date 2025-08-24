84b0f62f5febc7aa390eef5bec0666f514156351
Sakshith77 <sakshithking55@gmail.com>
2025-08-24 12:46:09 +0530

    error corrected 2

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+// src/app/contact/page.tsx
+
+export default function ContactPage() {
+  return (
+    <main>
+      <h1>Contact Page</h1>
+      <p>Welcome to the contact page of my site!</p>
+    </main>
+  )
+}
